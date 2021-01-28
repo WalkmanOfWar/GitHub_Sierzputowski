cea8ce085eb1f6122305c460de497c47d8a1d8a2
acer <wonverin@gmail.com>
2021-01-28 22:11:01 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/GitHub_Sierzputowski.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GitHub_Sierzputowski.dir/main.c.obj"
+  "GitHub_Sierzputowski.exe"
+  "GitHub_Sierzputowski.exe.manifest"
+  "GitHub_Sierzputowski.pdb"
+  "libGitHub_Sierzputowski.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GitHub_Sierzputowski.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/GitHub_Sierzputowski.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for GitHub_Sierzputowski.
+# This may be replaced when dependencies are built.
